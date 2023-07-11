20dbb15cb16c41847a0775ddb4d49d895ba92928
ViktoriiaNeroda <viktoriaaaneroda@gmail.com>
2023-07-11 22:05:54 +0300

    1

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -9,15 +9,11 @@ export const apithecatApi = axios.create({baseURL: 'https://api.thecatapi.com/v1
 
  export const fetchBreeds = () => {
   return apithecatApi.get(`/breeds/`).then(response => {
-      if (response.status !== 200) {
-        throw new Error(response.status);
-      }
-
+      
       return response.data;
     })
     .catch(error => {
       console.log(error);
-
       throw error;
     });
 };
@@ -26,31 +22,10 @@ export const fetchCatImg = breedId => {
   return apithecatApi
     .get(`/images/search?breed_ids=${breedId}`)
     .then(response => {
-      if (response.status !== 200) {
-        throw new Error(response.status);
-      }
-
-      return response.data;
+     
     })
-    .catch(error => {
-      Notiflix.Report.warning(
-        'Sorry, try again!',
-        {
-          width: '320px',
-          svgSize: '30px',
-          messageFontSize: '16px',
-          backgroundColor: 'purple',
-          warning: {
-            svgColor: '#f6c218',
-            titleColor: '#f6c218',
-            messageColor: '#f6c218',
-            buttonBackground: '#08aa31c2',
-            buttonColor: '#f6c218',
-            backOverlayColor: 'rgba(238,191,49,0.9)',
-          },
-        }
-      );
+     .catch(error => {
       console.log(error);
       throw error;
     });
-}; 
+};
