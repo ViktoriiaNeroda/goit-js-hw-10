3ae73f0738cdf21ce67669f8e7b7685c9d10616a
ViktoriiaNeroda <viktoriaaaneroda@gmail.com>
2023-07-11 22:22:02 +0300

    1

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -5,73 +5,25 @@ import Notiflix from 'notiflix';
  axios.defaults.headers.common['x-api-key'] =
   'live_5nVcm90UjcYCONd9Zs2kschF0jkgo3s2QaiMfSC29wag3Gq3MUs29a71K8YESRT7';
 
-export const apithecatApi = axios.create({baseURL: 'https://api.thecatapi.com'});
+const BASE_URL = 'https://api.thecatapi.com/v1';
 
- export const fetchBreeds = () => {
-  return apithecatApi
-    .get('/v1/breeds')
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error(response.status);
-      }
-
-      return response.data;
-    })
-    .catch(error => {
-      console.log(error);
-      Notiflix.Report.warning(
-        'THE MUSCOVITES BROKE IT ALL',
-        'but the Muscovites cannot defeat the Ukrainians. We believe in Ukrainian defenders',
-        'Ukraine will win',
-        {
-          width: '320px',
-          svgSize: '30px',
-          messageFontSize: '16px',
-          backgroundColor: '#e42525cd',
-          warning: {
-            svgColor: '#f6c218',
-            titleColor: '#f6c218',
-            messageColor: '#f6c218',
-            buttonBackground: '#08aa31c2',
-            buttonColor: '#f6c218',
-            backOverlayColor: 'rgba(238,191,49,0.9)',
-          },
-        }
-      );
-      throw error;
-    });
+export const fetchBreeds = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/breeds?`);
+    return response.data;
+  } catch (errors) {
+    console.error(errors);
+  }
 };
 
-
-export const fetchCatImg = breedId => {
-  return apithecatApi
-    .get(`/images/search?breed_ids=${breedId}`)
-    .then(response => {
-      if (response.status !== 200) {
-        throw new Error(response.status);
-      }
-
-      return response.data;
-    })
-    .catch(error => {
-      Notiflix.Report.warning(
-        'Sorry, try again!',
-        {
-          width: '320px',
-          svgSize: '30px',
-          messageFontSize: '16px',
-          backgroundColor: 'purple',
-          warning: {
-            svgColor: '#f6c218',
-            titleColor: '#f6c218',
-            messageColor: '#f6c218',
-            buttonBackground: '#08aa31c2',
-            buttonColor: '#f6c218',
-            backOverlayColor: 'rgba(238,191,49,0.9)',
-          },
-        }
-      );
-      console.log(error);
-      throw error;
-    });
-}; 
+//2
+export const fetchCatImg = async breedId => {
+  try {
+    const response = await axios.get(
+      `${BASE_URL}/images/search?&breed_ids=${breedId}`
+    );
+    return response.data;
+  } catch (errors) {
+    console.error(errors);
+  }
+};
