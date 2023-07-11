b7dfb51e326da41e961034c2fb0abeebdacda546
ViktoriiaNeroda <viktoriaaaneroda@gmail.com>
2023-07-11 22:17:05 +0300

    1

--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -5,7 +5,7 @@ import Notiflix from 'notiflix';
  axios.defaults.headers.common['x-api-key'] =
   'live_5nVcm90UjcYCONd9Zs2kschF0jkgo3s2QaiMfSC29wag3Gq3MUs29a71K8YESRT7';
 
-export const apithecatApi = axios.create({baseURL: 'https://api.thecatapi.com/'});
+export const apithecatApi = axios.create({baseURL: 'https://api.thecatapi.com'});
 
  export const fetchBreeds = () => {
   return apithecatApi
